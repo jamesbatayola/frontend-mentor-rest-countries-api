237e164f175be8bc2f9a683f38ce60539be25c63
Jake James Batayola <batayolajakejames@gmail.com>
2024-01-17 22:41:24 +0800

    60% done

--- script.js
+++ script.js
@@ -1,59 +1,130 @@
-const darkMode = document.querySelector(".dark-mode");
-
-//! Dark Mode --------------------------------
-
-const allText = document.querySelectorAll(".text");
-const allElement = document.querySelectorAll(".theme-changeable");
 const body = document.body;
 
-isLight = true;
+//! DROPDOWN --------------------------------
+const select = document.querySelector(".select");
+const dropDownMenu = document.querySelector(".dropdown");
+const dropDownItems = document.querySelectorAll(".drop");
 
-function checkTheme() {
-	if (isLight) {
-		darkTheme();
-	} else {
-		lightTheme();
-	}
-}
-
-function darkTheme() {
-	isLight = false;
-	allText.forEach((eachText) => {
-		eachText.style.color = "white";
-	});
-	allElement.forEach((eachElement) => {
-		eachElement.style.backgroundColor = "hsl(209, 23%, 22%)";
-	});
-	body.style.backgroundColor = "hsl(207, 26%, 17%)";
-}
-
-function lightTheme() {
-	isLight = true;
-	allText.forEach((eachText) => {
-		eachText.style.color = "black";
-	});
-	allElement.forEach((eachElement) => {
-		eachElement.style.backgroundColor = "white";
-	});
-	body.style.backgroundColor = "hsl(0, 0%, 98%)";
-}
-
-darkMode.addEventListener("click", checkTheme);
+select.addEventListener("click", () => {
+	select.classList.toggle("active");
+	dropDownMenu.classList.toggle("active");
+});
 
 //! API --------------------------------
 
 const cardContainer = document.querySelector(".cards-container");
-let data;
+// let data;
 
-async function initialCard() {
-	const fetched = await fetch("data.json");
-	const data = await fetched.json();
+// async function initialCard() {
+// 	const fetched = await fetch("data.json");
+// 	const data = await fetched.json();
+// 	console.log(data);
+
+// 	data.forEach((eachData) => {
+// 		const newDiv = document.createElement("div");
+// 		newDiv.classList.add("card");
+// 	});
+// }
+
+// initialCard();
+
+//* REGION
+
+async function getRegion(url) {
+	const link = await fetch(url);
+	data = await link.json();
 	console.log(data);
 
 	data.forEach((eachData) => {
-		const newDiv = document.createElement("div");
-		newDiv.classList.add("card");
+		const newCard = document.createElement("div");
+		newCard.classList.add("card", "theme-changeable");
+		newCard.innerHTML = `		           
+						<img class="card-flag" src="${eachData.flags.png}" alt="${eachData.flags.alt}" />
+						<div class="card-info">
+							<h2 class="card-name text">${eachData.name.common}</h2>
+							<div class="card-group">
+								<div class="card-item">
+									<h3 class="data populatoin text">Population:</h3>
+									<p class="text">${eachData.population}</p>
+								</div>
+								<div class="card-item">
+									<h3 class="data region text">Region:</h3>
+									<p class="text">${eachData.region}</p>
+								</div>
+								<div class="card-item">
+									<h3 class="data capital text">Capital:</h3>
+									<p class="text">${eachData.capital[0]}</p>
+								</div>
+							</div>
+						</div>
+					
+		`;
+		cardContainer.append(newCard);
+
+		//! dark mode for new cards -----------------
+		// checkTheme();
 	});
 }
 
-initialCard();
+dropDownMenu.addEventListener("click", (event) => {
+	cardContainer.innerHTML = "";
+	const target = event.target.id;
+
+	const url = `https://restcountries.com/v3.1/region/${target}`;
+	getRegion(url);
+});
+
+//! Dark Mode --------------------------------
+
+const darkMode = document.querySelector(".dark-mode");
+
+isLight = true;
+
+function checkTheme() {
+	const allText = document.querySelectorAll(".text");
+	const allElement = document.querySelectorAll(".theme-changeable");
+
+	if (isLight) {
+		isLight = false;
+		allText.forEach((eachText) => {
+			eachText.style.color = "white";
+		});
+		allElement.forEach((eachElement) => {
+			eachElement.style.backgroundColor = "hsl(209, 23%, 22%)";
+		});
+		body.style.backgroundColor = "hsl(207, 26%, 17%)";
+	} else {
+		isLight = true;
+		allText.forEach((eachText) => {
+			eachText.style.color = "black";
+		});
+		allElement.forEach((eachElement) => {
+			eachElement.style.backgroundColor = "white";
+		});
+		body.style.backgroundColor = "hsl(0, 0%, 98%)";
+	}
+}
+
+// function darkTheme() {
+// 	isLight = false;
+// 	allText.forEach((eachText) => {
+// 		eachText.style.color = "white";
+// 	});
+// 	allElement.forEach((eachElement) => {
+// 		eachElement.style.backgroundColor = "hsl(209, 23%, 22%)";
+// 	});
+// 	body.style.backgroundColor = "hsl(207, 26%, 17%)";
+// }
+
+// function lightTheme() {
+// 	isLight = true;
+// 	allText.forEach((eachText) => {
+// 		eachText.style.color = "black";
+// 	});
+// 	allElement.forEach((eachElement) => {
+// 		eachElement.style.backgroundColor = "white";
+// 	});
+// 	body.style.backgroundColor = "hsl(0, 0%, 98%)";
+// }
+
+darkMode.addEventListener("click", checkTheme);
