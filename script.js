f768e1aca67a97c1f64a417310f080b2056839b1
Jake James Batayola <batayolajakejames@gmail.com>
2024-01-18 23:53:44 +0800

    85% done

--- script.js
+++ script.js
@@ -17,28 +17,6 @@ const cardContainer = document.querySelector(".cards-container");
 
 //? CARD MAKER -----------------------------
 
-function addData(element, newCard) {
-	const languageRef = Object.keys(element.languages);
-	const currencyRef = Object.keys(element.currencies);
-	const countryCurrency = element.currencies[currencyRef[0]].name;
-	const countryNativeName = element.name && element.name.nativeName ? element.name.nativeName[languageRef[0]].common : "N / A";
-	const languageList = languageRef.map((each) => {
-		return element.languages[each];
-	});
-	// ! ---------------------
-	const newNativeName = countryNativeName,
-		newSubRegion = `${element.subregion}`,
-		newTopDomain = `${element.tld[0]}`,
-		newCurrency = countryCurrency,
-		newLanguage = languageList.join(" ");
-	// ! ---------------------
-	newCard.setAttribute("data-nativeName", newNativeName);
-	newCard.setAttribute("data-subRegion", newSubRegion);
-	newCard.setAttribute("data-topDomain", newTopDomain);
-	newCard.setAttribute("data-currency", newCurrency);
-	newCard.setAttribute("data-language", newLanguage);
-}
-
 function cardCreator(eachCard) {
 	const newCard = document.createElement("div");
 	newCard.classList.add("card", "theme-changeable");
@@ -70,6 +48,32 @@ function cardCreator(eachCard) {
 	const allNewText = document.querySelectorAll(".text");
 	const allNewElement = document.querySelectorAll(".theme-changeable");
 	checkTheme(allNewText, allNewElement);
+}
+
+function addData(element, newCard) {
+	const languageRef = Object.keys(element.languages);
+	const currencyRef = Object.keys(element.currencies);
+	const countryCurrency = element.currencies[currencyRef[0]].name;
+	const countryNativeName = element.name && element.name.nativeName ? element.name.nativeName[languageRef[0]].common : "N / A";
+	const languageList = languageRef.map((each) => {
+		return element.languages[each];
+	});
+
+	// ! ---------------------
+
+	const newNativeName = countryNativeName,
+		newSubRegion = `${element.subregion}`,
+		newTopDomain = `${element.tld[0]}`,
+		newCurrency = countryCurrency,
+		newLanguage = languageList.join(" ");
+
+	// ! ---------------------
+
+	newCard.setAttribute("data-nativeName", newNativeName);
+	newCard.setAttribute("data-subRegion", newSubRegion);
+	newCard.setAttribute("data-topDomain", newTopDomain);
+	newCard.setAttribute("data-currency", newCurrency);
+	newCard.setAttribute("data-language", newLanguage);
 }
 
 //? GET CARD BY REGION -----------------------------
@@ -115,6 +119,85 @@ async function searchName() {
 }
 
 searchIcon.addEventListener("click", getName);
+
+//! PREVIEW CARD =================================================================================
+
+const searchNav = document.querySelector(".search-nav");
+const cards = document.querySelectorAll(".card");
+const container = document.querySelector(".container");
+const main = document.querySelector("main");
+
+//? Creating a preview card
+
+cards.forEach((eachCard) => {
+	eachCard.addEventListener("click", () => {
+		console.log(eachCard);
+		main.style.display = "none";
+		const cardPreview = document.createElement("div");
+		cardPreview.classList.add("card-preview");
+		cardPreview.innerHTML = `
+	<div class="card-preview">
+	<div class="back-button">
+		<i class="text fa-solid fa-arrow-left-long"></i>
+		<p class="text">Back</p>
+	</div>
+	<section class="preview-main-container">
+		<img class="card-flag preview-flag" src="https://flagcdn.com/w320/de.png" alt="" />
+		<section class="card-details-container">
+			<article class="card-details-top">
+				<h2 class="text card-preview-name">Belgium</h2>
+				<div class="card-details-top-group">
+					<div class="details-item">
+						<h3 class="data text native-name">Native Name:</h3>
+						<p class="text">Belgie</p>
+					</div>
+					<div class="details-item">
+						<h3 class="data text population">Population:</h3>
+						<p class="text">11,319,511</p>
+					</div>
+					<div class="details-item">
+						<h3 class="data text region">Region:</h3>
+						<p class="text">Europe</p>
+					</div>
+					<div class="details-item">
+						<h3 class="data text sub-region">Sub Region:</h3>
+						<p class="text">Western Europe</p>
+					</div>
+					<div class="details-item">
+						<h3 class="data text capital">Capital:</h3>
+						<p class="text">Brussels</p>
+					</div>
+				</div>
+			</article>
+			<article class="card-details-bottom">
+				<div class="details-item">
+					<h3 class="data text tld">Top Level Domain:</h3>
+					<p class="text">.be</p>
+				</div>
+				<div class="details-item">
+					<h3 class="data text currencies">Currencies:</h3>
+					<p class="text">Euro</p>
+				</div>
+				<div class="details-item">
+					<h3 class="data text language">Languages:</h3>
+					<p class="text">Dutch, French, German</p>
+				</div>
+			</article>
+			<footer class="card-details-footer">
+				<h2 class="footer-heading">Border Countries:</h2>
+				<div class="borders-container">
+					<div class="borders-item border-1">France</div>
+					<div class="borders-item border-2">Germany</div>
+					<div class="borders-item border-3">Netherlands</div>
+				</div>
+			</footer>
+		</section>
+	</section>
+</div>
+`;
+		container.append(cardPreview);
+	});
+});
 
 //! Dark Mode ====================================================================================
 
